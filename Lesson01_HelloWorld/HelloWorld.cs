1be5dd3971a091d98733b5b760e56ca07f12c976
CodexPremiera <ashleyken.professional@gmail.com>
2024-06-10 20:40:22 +0800

    complete lesson 01 - introduction to c#

---
+++ Lesson01_HelloWorld/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Learn_C_Sharp.Lesson01_HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            // Hello World
+            Console.WriteLine("Hello C#!");
+            
+            // Reading console
+            Console.Write("How old are you? ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old");
+        }
+    }
+}
